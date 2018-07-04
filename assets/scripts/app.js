7468dd325a082b5171e79360cc6b7d4f7fc57b13
Igor Chernyshev <igorchern07@gmail.com>
2018-07-04 00:47:20 -0500

    local storage is now working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,44 @@
 (function() {
-  const topics = ['cat', 'dog', 'mouse', 'rabbit'];
+  const buttons = $('#buttons');
+  let topics = ['cat', 'dog', 'mouse', 'rabbit'];
+  const gifs = $('#gifs');
+  let newTopic;
+  let storedTopics;
   function renderButtons() {
+    storedTopics = JSON.parse(localStorage.getItem('storedTopics'));
+    console.log('stored: ', storedTopics);
+    if (storedTopics) {
+      topics = storedTopics;
+    }
+    console.log('topics; ', topics);
     topics.forEach(function(topic) {
-      $('#buttons').append('<button>' + topic + '</button>');
+      $('#buttons')
+        .append(`<button class="btn btn-outline-primary m-2">${topic}</button>`)
+        .attr('class', 'm-2');
     });
   }
+  $('.go').on('click', function(e) {
+    e.preventDefault();
+    let topicInput = $('.topic');
+    console.log(topicInput.val());
+    newTopic = topicInput.val().trim();
+    topicInput.val('');
+    if (topics.includes(newTopic.toLowerCase())) {
+      alert(`${newTopic} has already been added`);
+    } else if (newTopic) {
+      topics.push(newTopic.toLowerCase());
+
+      localStorage.setItem('storedTopics', JSON.stringify(topics));
+
+      buttons.empty();
+      renderButtons();
+    }
+  });
+  $('.clear').on('click', function() {
+    localStorage.clear();
+    topics = [];
+    buttons.empty();
+    renderButtons();
+  });
   renderButtons();
 })();
