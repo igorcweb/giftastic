bdc9c721ec4975c7cece44f5aa38403e80e569d1
Igor Chernyshev <igorchern07@gmail.com>
2018-07-04 21:00:55 -0500

    now able to start and start gif animation on click

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,15 @@
 (function() {
   const buttons = $('#buttons');
-  let topics = ['dog', 'cat', 'mouse', 'rabbit'];
+  let topics = ['dog', 'car', 'happy', 'turtle'];
   const gifs = $('#gifs');
   let newTopic;
   let storedTopics;
   function renderButtons() {
     storedTopics = JSON.parse(localStorage.getItem('storedTopics'));
-    console.log('stored: ', storedTopics);
     if (storedTopics) {
       topics = storedTopics;
     }
-    // topics.forEach(function(topic) {
+
     $.each(topics, function(index, topic) {
       let data = topic.replace(' ', '+');
       $('#buttons')
@@ -20,10 +19,10 @@
         .attr('class', 'm-2');
     });
   }
-  $('.go').on('click', function(e) {
+
+  function addButton(e) {
     e.preventDefault();
     let topicInput = $('.topic');
-    console.log(topicInput.val());
     newTopic = topicInput.val().trim();
     topicInput.val('');
     if (topics.includes(newTopic.toLowerCase())) {
@@ -34,32 +33,61 @@
       buttons.empty();
       renderButtons();
     }
-  });
-  $('.clear').on('click', function(e) {
+  }
+  function getGifs() {
+    let srcStill;
+    let srcAnimate;
+    $.get(url)
+      .then(res => {
+        $.each(res.data, function(index) {
+          srcStill = res.data[index].images.fixed_height_still.url;
+          srcAnimated = res.data[index].images.fixed_height.url;
+          gifs.prepend(
+            `<img src=${srcStill} class="img m-3" data-still=${srcStill} data-animated=${srcAnimated} data-state='still'>`
+          );
+        });
+        gifs.on('click', '.img', function() {
+          let $this = $(this);
+          if ($this.data('state') === 'still') {
+            $this.data('state', 'animated');
+            $this.attr('src', $this.data('animated'));
+            console.log($this.data('state'));
+            console.log($this.attr('src'));
+          } else {
+            $this.data('state', 'still');
+            $this.attr('src', $this.data('still'));
+            console.log($this.data('state'));
+            console.log($this.attr('src'));
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  $('.go').on('click', addButton);
+  $('.clear-topics').on('click', function(e) {
     e.preventDefault();
     localStorage.clear();
     topics = [];
     buttons.empty();
     renderButtons();
   });
+  $('.clear-gifs').on('click', function(e) {
+    e.preventDefault();
+    localStorage.clear();
+    gifs.empty();
+  });
   renderButtons();
   const api_key = () => {
-    return '5n53cDRx0FU49ewKdFwuBjKCTqy8XNip&limit=5';
+    return 'XzaARDwSgwOpZyQ8n6ZV2X61Cn5EkkRX';
   };
   let url;
   buttons.on('click', '.btn', function() {
     const query = this.dataset.topic;
-    url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${api_key()}`;
-    getGif();
+    url = `https://api.giphy.com/v1/gifs/search?q=${query}&limit=10&api_key=${api_key()}`;
+    console.log(url);
+    getGifs();
   });
-  function getGif() {
-    $.get(url)
-      .then(res => {
-        let src = res.data[0].images.original.url;
-        gifs.append(`<img src=${src}>`);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
 })();
