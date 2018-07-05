63a3606672df759a1bda80c029c0d62686932b79
Igor Chernyshev <igorchern07@gmail.com>
2018-07-04 23:16:05 -0500

    added an option to remove individual gifs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,16 @@
 (function() {
   const buttons = $('#buttons');
-  let topics = ['dog', 'car', 'happy', 'turtle'];
+  let topics = [
+    'dog',
+    'car',
+    'happy',
+    'turtle',
+    'sad',
+    'adventure',
+    'hiking',
+    'biking',
+    'music'
+  ];
   const gifs = $('#gifs');
   let newTopic;
   let storedTopics;
@@ -14,7 +24,7 @@
       let data = topic.replace(' ', '+');
       $('#buttons')
         .append(
-          `<button class="btn btn-outline-primary m-2" data-topic=${data}>${topic}</button>`
+          `<button class="btn btn-outline-primary m-md-2 my-2 mr-3" data-topic=${data}>${topic}</button>`
         )
         .attr('class', 'm-2');
     });
@@ -36,35 +46,56 @@
   }
   function getGifs() {
     let srcStill;
-    let srcAnimate;
+    let srcAnimated;
     $.get(url)
       .then(res => {
         $.each(res.data, function(index) {
           srcStill = res.data[index].images.fixed_height_still.url;
           srcAnimated = res.data[index].images.fixed_height.url;
           gifs.prepend(
-            `<img src=${srcStill} class="img m-3" data-still=${srcStill} data-animated=${srcAnimated} data-state='still'>`
+            `<div class='img d-inline-block'><img src=${srcStill} class="img m-3" data-still=${srcStill} data-animated=${srcAnimated} data-state='still'><i class="fas fa-times"></i></div>`
           );
-        });
-        gifs.on('click', '.img', function() {
-          let $this = $(this);
-          if ($this.data('state') === 'still') {
-            $this.data('state', 'animated');
-            $this.attr('src', $this.data('animated'));
-            console.log($this.data('state'));
-            console.log($this.attr('src'));
-          } else {
-            $this.data('state', 'still');
-            $this.attr('src', $this.data('still'));
-            console.log($this.data('state'));
-            console.log($this.attr('src'));
-          }
         });
       })
       .catch(err => {
         console.log(err);
       });
   }
+
+  gifs.on('click', '.img', function() {
+    let $this = $(this);
+    if ($this.data('state') === 'still') {
+      $this.data('state', 'animated');
+      $this.attr('src', $this.data('animated'));
+      console.log('state: ', $this.data('state'));
+      console.log('src: ', $this.attr('src'));
+    } else {
+      $this.data('state', 'still');
+      $this.attr('src', $this.data('still'));
+      console.log('state:', $this.data('state'));
+      console.log('src: ', $this.attr('src'));
+    }
+  });
+
+  gifs.on('mouseover', 'div.img', function() {
+    $(this)
+      .find('i')
+      .addClass('isShowing');
+  });
+  gifs.on('mouseout', 'div.img', function() {
+    $(this)
+      .find('i')
+      .removeClass('isShowing');
+  });
+
+  gifs.on('click', 'i', function(e) {
+    e.stopPropagation();
+    $(this)
+      .parent()
+      .removeClass('d-inline-block')
+      .addClass('d-none');
+    console.log($(this).parent());
+  });
 
   $('.go').on('click', addButton);
   $('.clear-topics').on('click', function(e) {
@@ -87,7 +118,6 @@
   buttons.on('click', '.btn', function() {
     const query = this.dataset.topic;
     url = `https://api.giphy.com/v1/gifs/search?q=${query}&limit=10&api_key=${api_key()}`;
-    console.log(url);
     getGifs();
   });
 })();
